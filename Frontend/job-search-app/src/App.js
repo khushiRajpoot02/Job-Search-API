5393e4d7320df22c94896282de7e8f26c3488919
khushiRajpoot02 <khushisinghbhu02@gmail.com>
2023-10-16 23:32:58 +0530

    learnet css props

--- Frontend/job-search-app/src/App.js
+++ Frontend/job-search-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
      <Header/>
      <Body/>
-     <Footer/>
+  
     </div>
   );
 }
--- Frontend/job-search-app/src/components/Body.js
+++ Frontend/job-search-app/src/components/Body.js
@@ -1,45 +1,61 @@
 import React from "react";
-
 function Body() {
   return (
-    <div className="body">
+    <>
+    <div id="main-content">
       <div className="left">
-        <span>LET YOUR</span>
-        <br />
-        <span>MIND EXPLORE</span>
-        <br />
-        <span>NEW WORLD</span>
-        <br />
+        <div className="big-text">
+          <span>LET YOUR</span>
+          <br />
+          <span className="gradient-text">MIND EXPLORE</span>
+          <br />
+          <span>NEW WORLD</span>
+          <br />
+        </div>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry <br />
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry..
         </p>
-        <div className="btn">
-          <div className="btn1">BUY NOW</div>
-          <div className="btn2">EXPLORE</div>
+        <div className="butons">
+          <div className="btn btn1">BUY NOW</div>
+          <div className="btn">EXPLORE</div>
         </div>
-        <div className="overview-content">
-          <h4>SOMETHING</h4>
-          <p> haahh hahhh hahahahahhaah hahahh</p>
-        </div>
-        <div className="overview-content">
-          <h4>SOMETHING</h4>
-          <p> haahh hahhh hahahahahhaah hahahh</p>
-        </div>
-        <div className="overview-content">
-          <h4>SOMETHIN</h4>
-          <p> haahh hahhh hahahahahhaah hahahh</p>
+        <div className="overview">
+          <div className="overview-items">
+            <h4>500+</h4>
+            <p> haahh hahhh</p>
+          </div>
+          <div className="overview-items">
+            <h4>400+</h4>
+            <p> haahh hahhh</p>
+          </div>
+          <div className="overview-items">
+            <h4 className="gradient-text">200+</h4>
+            <p> haahh hahhh</p>
+          </div>
         </div>
       </div>
       <div className="right">
         <img
-          src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80 "
+          src="https://img.etimg.com/thumb/width-640,height-480,imgsize-946742,resizemode-75,msid-99960905/industry/services/education/education-and-the-future-of-generative-ai.jpg"
           alt="imgage"
         />
       </div>
     </div>
+    <div className="band">
+        <div>
+          <h3>Band1</h3>
+        </div>
+        <div>
+          <h3>Band2</h3>
+        </div>
+        <div>
+          <h3>Band3</h3>
+        </div>
+      </div>
+    </>
   );
 }
 export default Body;
