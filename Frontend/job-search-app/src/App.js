b4bf8293258ff7eb2f0485b97f73e8ee51c5295f
khushiRajpoot02 <khushisinghbhu02@gmail.com>
2023-10-17 23:41:14 +0530

    styled card

--- Frontend/job-search-app/src/App.js
+++ Frontend/job-search-app/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import Main from './components/Main'
 function App() {
   return (
     <div className="App">
      <Header/>
      <Body/>
-  
+  <Main/>
     </div>
   );
 }
---
+++ Frontend/job-search-app/src/components/Main.js
@@ -0,0 +1,85 @@
+import React from "react";
+function Main() {
+  return (
+    <>
+      <section className="sub-main">
+        <p className="heading">
+          <h2>
+            CHOSE YOUR
+            <br />
+            <span className="gradient-text">FAVOURITE</span>
+          </h2>
+        </p>
+        <div className="description">
+          <p>there are text written so just write it like this</p>
+        </div>
+        <div className="carousel">
+          <img
+            className="images"
+            src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8311692b-ad9a-433f-9521-74d239c9a589/dgb7770-47b4b65d-1f63-4d36-a031-82f467aba763.jpg/v1/fill/w_644,h_350,q_70,strp/eclipsa_by_lamona42_dgb7770-350t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzgzMTE2OTJiLWFkOWEtNDMzZi05NTIxLTc0ZDIzOWM5YTU4OVwvZGdiNzc3MC00N2I0YjY1ZC0xZjYzLTRkMzYtYTAzMS04MmY0NjdhYmE3NjMuanBnIiwiaGVpZ2h0IjoiPD02OTYiLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS53YXRlcm1hcmsiXSwid21rIjp7InBhdGgiOiJcL3dtXC84MzExNjkyYi1hZDlhLTQzM2YtOTUyMS03NGQyMzljOWE1ODlcL2xhbW9uYTQyLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.2FUUzBpTn0brI045QDAbf-bpmCRQ1_-VkS1bFW75FsM"
+            alt=" img1"
+          ></img>
+          <img
+            className="images image-center"
+            src="https://img.freepik.com/premium-photo/woman-s-face-is-made-up-geometric-shapes-cyberpunk-colorful-fractalism-cubism_834088-1.jpg"
+          />
+          <img
+            className="images"
+            src="https://img.freepik.com/free-photo/multi-colored-creativity-close-up-human-eye-generated-by-ai_188544-15574.jpg"
+          />
+        </div>
+        <div className="butons">
+          <div className="btn btn1">BUY NOW</div>
+          <div className="btn">EXPLORE</div>
+        </div>
+        <section className="discover-section">
+          <div className="discover-card">
+            <div className="discover-image">
+              <img
+                src="https://media.istockphoto.com/id/521577939/photo/advance-of-inner-paint.jpg?s=612x612&w=0&k=20&c=cUO1rlOng7aqJPXUrCiG0KmC-9y7LgCxkYSuj2tRito="
+                alt=""
+              ></img>
+            </div>
+            <div className="disc-descri">
+              <p className="heading">
+                <h2>
+                  CHOSE YOUR
+                  <br />
+                  <span className="gradient-text">FAVOURITE</span>
+                </h2>
+              </p>
+              <div className="description">
+                <p>there are text written so just write it like this</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="sub-cards">
+        <div className="butons">
+        <div className="btn btn1">BUY NOW</div>
+        <div className="btn">EXPLORE</div>
+        <div className="btn">EXPLORE</div>
+        <div className="btn">EXPLORE</div>
+      </div>
+        <div className="cards-items">
+        <div className="demo-card">
+        <div className="card-img">
+        <img src='https://media.istockphoto.com/id/521577939/photo/advance-of-inner-paint.jpg?s=612x612&w=0&k=20&c=cUO1rlOng7aqJPXUrCiG0KmC-9y7LgCxkYSuj2tRito='/>
+        </div>
+        <h3 className="card-title">Hii this is card</h3>
+        <div className="card-sub-img">
+        <img src='https://media.istockphoto.com/id/521577939/photo/advance-of-inner-paint.jpg?s=612x612&w=0&k=20&c=cUO1rlOng7aqJPXUrCiG0KmC-9y7LgCxkYSuj2tRito='></img>
+        <h4 className="main-title">Title</h4>
+        <p className="card-description">Hii this is all about the <br/>img and hope you will like it</p>
+        </div>
+        <div className="card-btn">
+        <div className="card-btn1">Explore</div>
+        </div>
+        </div>
+        </div>
+        </section>
+      </section>
+    </>
+  );
+}
+export default Main;
